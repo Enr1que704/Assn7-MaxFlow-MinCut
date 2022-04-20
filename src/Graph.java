00d186e846ffe026199ffa7daf3994857b553bbf
Eric Johnson <ericdavidjohns@gmail.com>
2022-04-20 16:35:34 -0600

    finished maxflow

--- src/Graph.java
+++ src/Graph.java
@@ -1,9 +1,10 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Graph {
-    private final GraphNode[] vertices;  // Adjacency list for graph.
+    private final GraphNode[] vertices;  // Adjacency list for graph. NOTE: I didn't delete this as it is used further down
     private final String name;  //The file from which the graph was created.
-    private GraphNode[] residual;
+    private final GraphNode[] residual;
 
     public Graph(String name, int vertexCount) {
         this.name = name;
@@ -36,8 +37,13 @@ public class Graph {
      * Algorithm to find max-flow in a network
      */
     public int findMaxFlow(int s, int t, boolean report) { // parent is from, v is to
+        System.out.println();
+        System.out.println("-- Max Flow: " + name + " --");
+
         int totalFlow = 0;
         while (hasAugmentingPath(s, t)) {
+            ArrayList<Integer> path = new ArrayList<>();
+
             int availableFlow = 100000;
             GraphNode v = residual[t];
             while (v.parent != -1) {
@@ -47,10 +53,13 @@ public class Graph {
                     if (v.successor.get(i).to == prevNode.id) { // if the successor of the current edge is equal to where we just were
                         if (v.successor.get(i).capacity < availableFlow) { // if the edge capacity is smaller than current available flow
                             availableFlow = v.successor.get(i).capacity;
+
                         }
                     }
                 } /* END OF FOR LOOP */
             }
+            System.out.print("Flow " + availableFlow + ": ");
+            path.add(t);
             v = residual[t];
             while (v.parent != -1) {
                 GraphNode prevNode = v;
@@ -58,17 +67,35 @@ public class Graph {
                 for (int i = 0; i < v.successor.size(); i++) {
                     if (v.successor.get(i).to == prevNode.id) {
                         v.successor.get(i).capacity -= availableFlow;
+                        path.add(v.id);
                     }
                 }
-                for (int i = 0; i < prevNode.successor.size(); i++) { // THIS IS WRONG
+                for (int i = 0; i < prevNode.successor.size(); i++) {
                     if (prevNode.successor.get(i).to == v.id) {
                         prevNode.successor.get(i).capacity += availableFlow;
                     }
                 }
             }
+            /* Print out the path that contains the flow */
+            for (int i = path.size() - 1; i > 0; i--) {
+                System.out.print(path.get(i) + " ");
+            }
+            System.out.print(t);
+            System.out.println();
             totalFlow += availableFlow;
         }
-
+        System.out.println();
+        /* Print the edges */
+        for (int i = 0; i < residual.length; i++) {
+            for (int j = 0; j < residual[i].successor.size(); j++) {
+                int vertCapacity = vertices[i].successor.get(j).capacity;
+                int residCapacity = residual[i].successor.get(j).capacity;
+                /* For each of the capacities that aren't 0 and have been changed */
+                if (vertCapacity > 0 && vertCapacity != residCapacity) {
+                    System.out.println("Edge(" + residual[i].id + ", " + residual[i].successor.get(j).to + ") transports " + (vertCapacity - residCapacity) + " items");
+                }
+            }
+        }
         return totalFlow;
     }
 
@@ -102,7 +129,8 @@ public class Graph {
      * Algorithm to find the min-cut edges in a network
      */
     public void findMinCut(int s) {
-        // TODO:
+        // TODO: WRITE THIS METHOD LIKE A CODING CHAD
+
     }
 
     public String toString() {
